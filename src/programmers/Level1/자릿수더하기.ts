9d7be40567014220632fb25b4b1ba6c40033111e
kimmiae <32053142+KIMMIAE@users.noreply.github.com>
2022-10-05 21:14:04 +0900

    fixed wrong code bug.

--- src/programmers/Level1/자릿수더하기.ts
+++ src/programmers/Level1/자릿수더하기.ts
@@ -1,11 +1,9 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12931
 function solution12931(n: number) {
     let nums = String(n).split('');
-    console.log(nums)
-    let answer = nums.reduce((curr, acc) => {
-        return Number(acc) + Number(curr);
+    let answer = nums.reduce((acc, curr) => {
+        return acc + Number(curr);
     }, 0);
-
 
     return answer;
 }
